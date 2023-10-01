0c5e732aa15ce7bb5c9a8ce3f3d8fc888df71e26
Nikita Ryabov <xleo77777@gmail.com>
2023-10-01 16:37:30 +0300

    Добавьте файлы проекта.

---
+++ VGTUlab1/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace VGTUlab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.closeButton = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.panel1.Controls.Add(this.closeButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(260, 29);
+            this.panel1.TabIndex = 0;
+            // 
+            // closeButton
+            // 
+            this.closeButton.AutoSize = true;
+            this.closeButton.BackColor = System.Drawing.Color.Red;
+            this.closeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.closeButton.ForeColor = System.Drawing.Color.Cornsilk;
+            this.closeButton.Location = new System.Drawing.Point(230, -1);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(30, 29);
+            this.closeButton.TabIndex = 0;
+            this.closeButton.Text = "X";
+            this.closeButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(237)))), ((int)(((byte)(237)))));
+            this.label1.Location = new System.Drawing.Point(18, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Выберите задание.";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(194)))), ((int)(((byte)(194)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(236)))), ((int)(((byte)(236)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(16, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Задание 1.";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(194)))), ((int)(((byte)(194)))));
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(236)))), ((int)(((byte)(236)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(16, 144);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 29);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Задание 2.";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(194)))), ((int)(((byte)(194)))));
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(236)))), ((int)(((byte)(236)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(16, 198);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(166, 29);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Задание 3.";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(194)))), ((int)(((byte)(194)))));
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(236)))), ((int)(((byte)(236)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(16, 252);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(166, 29);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Задание 4.";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(260, 308);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label closeButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ VGTUlab1/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+struct Teacher
+{
+    public string Name;
+    public string Subject;
+    public int Age;
+
+    // Метод для отображения данных о преподавателе
+    public void DisplayTeacherInfo()
+    {
+        Console.WriteLine($"Имя: {Name}");
+        Console.WriteLine($"Предмет: {Subject}");
+        Console.WriteLine($"Возраст: {Age} лет");
+    }
+}
+
+namespace VGTUlab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            string input = Console.ReadLine();
+            double x, y, z;
+            x = Double.Parse(input);
+            input = Console.ReadLine();
+            y = Double.Parse(input);
+            input = Console.ReadLine();
+            z = Double.Parse(input);
+            double a, b;
+            a = (1 + y) * ((x + (y / (Math.Pow(x, 2) + 4))) / (Math.Pow(2.7, 2) + (1 / (Math.Pow(x, 2) + 4))));
+            b = (1 + Math.Pow(Math.Cos(y - x), 3)) / ((Math.Pow(x, 2) / 2) + Math.Pow(Math.Sin(z), 2));
+            Console.WriteLine(x + " " + y + " " + z);
+            Console.WriteLine(a + " " + b);
+            double x1 = 1, y1 = 1, x2 = 5, y2 = 4, x3 = 7, y3 = 1;
+            double S, P;
+            double a1, b1, c1;
+            P = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)) + Math.Sqrt(Math.Pow((x3 - x2), 2) + Math.Pow((y3 - y2), 2)) + Math.Sqrt(Math.Pow((x3 - x1), 2) + Math.Pow((y3 - y1), 2));
+            S = Math.Sqrt(P / 2 * (P / 2 - Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2))) * (P / 2 - Math.Sqrt(Math.Pow((x3 - x2), 2) + Math.Pow((y3 - y2), 2))) * (P / 2 - Math.Sqrt(Math.Pow((x3 - x1), 2) + Math.Pow((y3 - y1), 2))));
+            Console.WriteLine(S + " " + P);
+            ThirdTask();
+            FourthTask();
+            Teacher teacher1;
+            teacher1.Name = "Иван Иванов";
+            teacher1.Subject = "Математика";
+            teacher1.Age = 35;
+
+            // Вызываем метод отображения данных
+            Console.WriteLine("Данные о преподавателе:");
+            teacher1.DisplayTeacherInfo();
+        }
+        static void ThirdTask()
+        {
+            Console.Write("Введите координату x: ");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите координату y: ");
+            double y = double.Parse(Console.ReadLine());
+
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("Точка находится в первой четверти.");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("Точка находится во второй четверти.");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("Точка находится в третьей четверти.");
+            }
+            else if (x > 0 && y < 0)
+            {
+                Console.WriteLine("Точка находится в четвёртой четверти.");
+            }
+            else if (x == 0 && y != 0)
+            {
+                Console.WriteLine("Точка лежит на оси Y.");
+            }
+            else if (x != 0 && y == 0)
+            {
+                Console.WriteLine("Точка лежит на оси X.");
+            }
+            else
+            {
+                Console.WriteLine("Точка лежит в начале координат.");
+            }
+        }
+        static void FourthTask()
+        {
+            Console.Write("Введите значение n >> ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n == 1)
+            {
+                Console.Write("Введите значение a >> ");
+                double a = double.Parse(Console.ReadLine());
+                Console.Write("Введите значение b >> ");
+                double b = double.Parse(Console.ReadLine());
+                Console.Write("Введите значение c >> ");
+                double c = double.Parse(Console.ReadLine());
+                Console.Write("Введите значение x >> ");
+                double x = double.Parse(Console.ReadLine());
+                Console.WriteLine(a*Math.Pow(x,2) + b*x+ c);
+            }
+            else if (n == 2)
+            {
+                Console.Write("Введите значение a >> ");
+                double a = double.Parse(Console.ReadLine());
+                Console.Write("Введите значение b >> ");
+                double b = double.Parse(Console.ReadLine());
+                Console.Write("Введите значение x >> ");
+                double x = double.Parse(Console.ReadLine());
+                Console.WriteLine(a * Math.Pow(x, 3) + 3* b * x + Math.Pow(b, 3));
+            }
+            else if (n ==3)
+            {
+                Console.Write("Введите значение x >> ");
+                double x = double.Parse(Console.ReadLine());
+                Console.WriteLine(4 * Math.Pow(Math.Sin(x), 2) + Math.Pow(Math.Cos(x), 2));
+            }
+            else if (n == 4)
+            {
+                Console.Write("Введите значение x >> ");
+                double x = double.Parse(Console.ReadLine());
+                Console.Write("Введите значение x >> ");
+                double y = double.Parse(Console.ReadLine());
+                Console.WriteLine(Math.Sqrt(Math.Abs(x-y) * Math.Pow(Math.Cos(x), 2)));
+            }
+            else if (n == 5)
+            {
+                Console.Write("Введите значение x >> ");
+                double x = double.Parse(Console.ReadLine());
+                Console.WriteLine(1.0 / Math.Pow(Math.Tan(x),2));
+            }
+            else
+            {
+                Console.WriteLine("Неправильно введён n.");
+            }
+        }
+    }
+}
